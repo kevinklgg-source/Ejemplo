f978869b22b2cd8d83ddaa8346657988f81af872
kevinklgg-source <kevin.klgg@gmail.com>
2026-06-10 18:13:37 -0600

    Update Calculadora Básica.py

--- Proyecto Final/Calculadora Básica.py
+++ Proyecto Final/Calculadora Básica.py
@@ -4,9 +4,11 @@
 
 import tkinter as tk
 
+# Función para agregar el número o operador al campo de entrada
 def agregar_numero(numero):
     entrada.insert(tk.END, numero)
 
+# Funcuión para realizar las operaciones aritmeticas básicas (+, -, * y /)
 def realizar_operaciones():
     try:
         resultado = eval(entrada.get())
@@ -16,15 +18,19 @@ def realizar_operaciones():
         entrada.delete(0, tk.END)
         entrada.insert(tk.END, "Error")
 
+# Función para limpiar el campo de entrada
 def limpiar():
     entrada.delete(0, tk.END)
 
+# Creación de la ventana principal
 ventana = tk.Tk()
 ventana.title("Calculadora")
 
+# Realizar el campo de entrada
 entrada = tk.Entry(ventana, font=("Arial", 20), justify="right")
 entrada.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
+# Lista de botones con sus respectivos valores y la ubicación en la cuadrícula
 botones = [
     ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("/", 1, 3),
     ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("*", 2, 3),
@@ -32,6 +38,7 @@ botones = [
     ("0", 4, 0), (".", 4, 1), ("+", 4, 3)
 ]
 
+# Ahora los botones y ubicamos la cuadrícula
 for valor, fila, columna in botones:
     boton = tk.Button(
         ventana,
@@ -41,6 +48,7 @@ for valor, fila, columna in botones:
     )
     boton.grid(row=fila, column=columna, padx=5, pady=5, sticky="nsew")
 
+# Creación de el botón para limpiar el campo de entrada
 limpiar_boton = tk.Button(
     ventana,
     text="C",
@@ -49,6 +57,7 @@ limpiar_boton = tk.Button(
 )
 limpiar_boton.grid(row=5, column=0, padx=5, pady=5, columnspan=2, sticky="nsew")
 
+# Creación de el botón para obtener el resultado
 calcular_boton = tk.Button(
     ventana,
     text="=",
@@ -57,4 +66,5 @@ calcular_boton = tk.Button(
 )
 calcular_boton.grid(row=5, column=2, padx=5, pady=5, columnspan=2, sticky="nsew")
 
+# Iniciamos el bucle principal de la aplicación
 ventana.mainloop()
